af0a2db74323bd3d4190bbaecceaa3e43997006b
Jared <jared.online@gmail.com>
2009-02-13 00:18:37 -0700

    adding validation

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,5 +1,7 @@
 class BandsController < ApplicationController
   layout 'application'
+  require 'hpricot'
+  require 'open-uri'
   
   make_resourceful do
     actions :all
@@ -8,6 +10,40 @@ class BandsController < ApplicationController
   def index
     @winner = Band.find(:first, :order => "num_votes DESC")
     @bands = Band.find(:all, :conditions => "id != #{@winner.id}", :order => "num_votes DESC")
+  end
+  
+  def create
+    @band = Band.new(params[:band])
+    
+    band_names = []
+    doc = Hpricot.XML(open('http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + @band.name.parameterize.to_str + '&api_key=' + LASTFM_API_KEY))
+    
+    (doc/"artist/name").each do |artist_name|
+       band_names << artist_name.inner_html
+    end
+    
+    
+      if band_names.include?(@band.name)
+       unless Band.exists?(:name => @band.name)
+          if @band.save
+            flash[:notice] = 'Band was successfully created.'
+            redirect_to(@band)
+            # format.xml  { render :xml => @band, :status => :created, :location => @band }
+          else
+            flash[:error] = "Unable to save band for some reason."
+            render :action => "new"
+            # format.xml  { render :xml => @band.errors, :status => :unprocessable_entity }
+          end
+        else
+          @band = Band.find_by_name(params[:band][:name])
+          redirect_to up_band_path(@band)
+        end
+      else
+        flash[:error] = "That's not a real band."
+        @suggestions = band_names
+        render :action => "new"
+      end
+    
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :tests
+
   # The priority is based upon order of creation: first created -> highest priority.
   
   map.resources :bands, :member => {:up => :get, :down => :get} do |band|
