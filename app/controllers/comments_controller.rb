782cef8a33ec4cc80e637729355076bf478adc72
Jared <jared.online@gmail.com>
2009-02-13 21:58:31 -0700

    more css tweaks, and create comment function enabled

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
   
-  make_resourceful do
-    actions :all
-    belongs_to :band
+  def create
+    @band = Band.find_by_permalink(params[:band_id])
+    @comment = @band.comments.new(params[:comment])
+    
+    if @comment.save
+      redirect_to band_path(@comment.band)
+    else
+      flash[:error] = "Couldn't save your comment for some reason.  Sorry 'bout that."
+      redirect_to band_path(@comment.band)
+    end
   end
   
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :band
+  
+  def link
+    if self.website != ''
+      website
+    elsif email != ''
+      'mailto:' + email
+    end
+  end
 end
